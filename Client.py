48c8af0d088bf89c8d8a439b99f716757fadceee
Ben <benjaminjknight0987@gmail.com>
2020-11-23 13:51:07 -0600

    changing controller scheme

--- Client.py
+++ Client.py
@@ -5,7 +5,10 @@ Gregory Ferguson
 import socket
 import threading
 import ast
+import time
 from inputs import get_gamepad
+import pygame
+from pygame.locals import *
 
 
 #HOST = '192.168.43.179'
@@ -14,61 +17,114 @@ HOST = 'localhost'
 controller_command = 'A'
 
 def send(threadName, socket):
-    
-    
+
+    pygame.init()
+
+    joysticks = []
+
+    for i in range(pygame.joystick.get_count()):
+        joysticks.append(pygame.joystick.Joystick(i))
+        joysticks[-1].init()
+
+    controller = joysticks[0]
+    reverse = False
+
+    while True:
+        pygame.event.pump()
+
+        if(controller.get_button(6)):
+            print("Exiting...")
+            exit()
+
+        # divider ensure only half power given unless "A" button pressed down
+        divider = 0.125
+        reverse = 0
+        right_power =   controller.get_axis(5)
+        left_power =    controller.get_axis(2)
+
+        # "A" button pressed, give full power
+        if(controller.get_button(0)):
+            divider =  0.25
+
+        right_power =   divider * (right_power + 1)
+        left_power =    divider * (left_power + 1)
+
+        # Bumpers are pressed, go reverse. Must be doing both to avoid breaking bot
+        if(controller.get_button(4) and controller.get_button(5)):
+            left_power =    0.5 - left_power
+            right_power =   0.5 - right_power
+            reverse = 1
+        else:
+            left_power =    0.5 + left_power
+            right_power =   0.5 + right_power
+
+        # construct packet string array
+        data = [str(round(left_power, 2)), str(round(right_power, 2)), str(reverse)]
+
+        print(data)
+
+        sent = socket.send(str(data).encode('utf8'))
+
+        if(sent == 0):
+            raise RuntimeError("socket connection broken")
+
+        time.sleep(.1)
+        del data[:]
+
+    """
      while True:
         events = get_gamepad()
         for event in events:
-            
+
             if(event.ev_type != "Sync"):
 #                print(event.code, event.state)
 #                print()
-                
+
                 data = [controller_command, event.code, event.state]
-                
+
                 print(data)
-                
+
                 sent = socket.send(str(data).encode('utf8'))
-                
+
                 if(sent == 0):
                     raise RuntimeError("socket connection broken")
-                
+
                 del data[:]
-        
-    
+    """
+
 
 def receive(threadName, socket):
-    
+
     data = socket.recv(1024)
-    
+
     strings = data.decode('utf8')
-    
-    #Converting string to list 
-    res = strings.strip('][').split(', ') 
-    
+
+    #Converting string to list
+    res = strings.strip('][').split(', ')
+
     print(res)
-    
+
     #At this point, will check to see what the data from server is and update the GUI
-    
-    
+
+
 def main():
-    
-    
+
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
+
     s.connect((HOST, 5220))
-    
+
     print("Connected")
-    
+
 
 #    t2 = threading.Thread(target = receive, args = ("Thread-2", s))
 #
 #    t2.start()
-    
+
     t1 = threading.Thread(target = send, args = ("Thread-1", s))
     t1.start()
     t1.join()
-     
+
 
 if __name__=="__main__":
     main()
--- Server.py
+++ Server.py
@@ -34,169 +34,174 @@ ser = 0
 """
 TODO
 
-Need to implement a standard between server and arduino to allow for communication. 
+Need to implement a standard between server and arduino to allow for communication.
 """
 
 
 def Serial_Send(data, ser):
-    
-    
+
+
     #print("NA")
     #At this point, need to use struct to pack that data into bytes
-    
-    
-    #Once you have the data in bytes, send to embedded system. 
+
+
+    #Once you have the data in bytes, send to embedded system.
     encoded_data = []
-    
+
     for i in range(0, len(data)):
         length = len(data[i])
         data[i] = data[i].encode('utf-8')
         encoded_data.append(struct.pack(str(length) + 's', data[i]))
-    
+
     print(encoded_data)
-       
+
 def Serial_Receive(ser):
-    
+
     time.sleep(5)
     #print("NA")
-    
+
     #t = threading.Thread(target = Client_Send, args = (clientsocket, data))
-    
+
     #t.start()
-    
+
     while True:
         x = ser.read()
-        
+
         #x = x.decode("utf-8")
-        
+
         print(x)
-    
+
     ser.close()
 
 #This should only be called inside Serial_Receive for now
 #Later on, other functions may call it, but it should never always be running
 def Client_Send(clientsocket, data):
-    
-    
+
+
     print("NA")
-    
+
 #Easier to understand than binary data, send strings between server and client.
-#Then convert to list to be used later on, data can be any type. 
+#Then convert to list to be used later on, data can be any type.
 def Client_Receive(clientsocket):
     global ser
     counter = 0
     while (counter < 1000):
-        
+
         # no forward or reverse command, stop
         motor_toggle_pin.write(0)
         l_motor_pin.write(.49804)
         r_motor_pin.write(.49804)
-        
+
         counter += 1
-    
+
         data = (clientsocket.recv(1024))
-        
+
         strings = data.decode('utf8')
-        
-        # Converting string to list 
-        res = strings.strip('][').split(', ') 
-        
-        data = [res[0], res[1]]
-        
-        text = data[1].strip()
-        print(text)
-        
-        if (text == "'ABS_RZ'"):
+
+        # Converting string to list
+        res = strings.strip('][').split(', ')
+
+        data = [res[0], res[1], res[2]]
+
+        if(len(strings) > 20):
+            continue
+
+        #print(res[0].strip("'"))
+
+        l_value =  float(res[0].strip("'"))
+        r_value =  float(res[1].strip("'"))
+
+        if (res[2] == "'0'"):
             motor_toggle_pin.write(1)
-            l_motor_pin.write(1)
-            r_motor_pin.write(1)
+            l_motor_pin.write(l_value)
+            r_motor_pin.write(r_value)
             time.sleep(.05)
-            print("forwards")
+            print(f"forwards, L = {l_value} R = {r_value}")
             #send value/2 +28
-            
-        elif (text == "'ABS_Z'"):
+
+        else:
             motor_toggle_pin.write(1)
-            l_motor_pin.write(0)
-            r_motor_pin.write(0)
+            l_motor_pin.write(l_value)
+            r_motor_pin.write(r_value)
             time.sleep(.05)
-            print("backwards")
+            print(f"backwards, L = {l_value} R = {r_value}")
             #send value/2
-    
-            
-    
+
+        strings = ""
+
     #At this point, need to check first index (res[0]) to determine what the packet data is for
-    #e.g. drive commands, arm commands, etc. 
-    
-    
+    #e.g. drive commands, arm commands, etc.
+
+
 
 def main():
-    
-    
+
+
     #################################
     # Server and Client Setup
     ##################################
-    
+
     # Using IPv4 with a TCP socket
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
+
     # bind the socket to a public host, and a well-known port
     serversocket.bind((HOST, 5220))
     # become a server socket
     serversocket.listen(5)
-    
+
     clientsocket, address = serversocket.accept()
-        
-    
+
+
     t1 = threading.Thread(target = Client_Receive, args = (clientsocket,))
-   
+
     t1.start()
-    
-    
-    
+
+
+
     #Once server and client are connected, will try to connect to a serial device.
     #The rest of the system will still work if this fails
     #Will attempt to connect until successful
-    
+
     ###################################
     # Embedded System Setup
     ###################################
-    
-    
- 
-#    
+
+
+
+#
 #    global ser
 #    ser = serial.Serial()
 #    ser.baudrate = com_port_baudrate
 #    ser.port = com_port
-#    ser.open() 
+#    ser.open()
 #    ser.flushInput()
-#    
+#
 #    print(ser)
-#    
-#    
-#    
+#
+#
+#
 #    if(ser.is_open != True):
 #       # print("NA")
 #        ser.open()
-#    
+#
 #    if(ser.is_open == True):
-#        
+#
 #        print("Port open")
 #        #If succesful in connecting and opening the com port, start threads
-#                
-#        
-#        
+#
+#
+#
 #        t2 = threading.Thread(target = Serial_Receive, args = ( ser,))
 #        t2.start()
-    
-    #Else, try to connect again. 
-    
+
+    #Else, try to connect again.
+
 #    data = ["XR", "220"]
-#    
+#
 #     #Send controller data to embedded system
 #    t = threading.Thread(target = Serial_Send, args = (data, ser))
 #    t.start()
-    
-       
+
+
 if __name__=="__main__":
     main()
